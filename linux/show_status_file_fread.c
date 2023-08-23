84ebc8b0a640fc11ec663cbcd22e50354c3ee72c
Fr0mMars <54331466+Fr0mMars@users.noreply.github.com>
2023-08-23 19:16:02 +0300

    Add files via upload

---
+++ linux/show_status_file_fread.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 256
+
+int main(int argc, char *argv[])
+{
+    int len;
+    char buf[BUF_SIZE];
+    FILE *f = NULL;
+
+    if ((f = fopen("/proc/self/status", "r")) == NULL)
+    {
+        printf("Cannot open file.\n");
+        exit(-1);
+    }
+    while ((len = fread(buf, sizeof(char), BUF_SIZE - 1, f)) > 0)
+    {
+        for (int i = 0; i < len; i++)
+            if (0 == buf[i])
+            {
+                buf[i] = 10;
+            }
+        buf[len] = 0;
+        printf("%s", buf);
+    }
+    if (feof(f))
+    {
+        printf("Premature end of file.\n");
+    }
+    else
+        printf("File read error.\n");
+    fclose(f);
+
+    return 0;
+}
